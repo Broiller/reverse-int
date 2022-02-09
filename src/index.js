a5ab3df082da948dbb126fbb7f568ce986d007a8
Yauheni Haurylenka <djonkaupervud@mail.ru>
2022-02-09 20:55:41 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function reverse (n) {
-  
+
+    let reverseNumber = 0;
+    n = checkNegative(n);
+
+    while (n >= 1) {
+        let lastNumber = n % 10;
+        reverseNumber = (reverseNumber * 10) + lastNumber;
+        n = Math.trunc(n / 10);
+    }
+    return reverseNumber;
 }
+
+const checkNegative = (n) => {
+    if (n < 0) { 
+        n = Math.abs(n); 
+    }
+    return n;
+}
+
+
+
+
+    
+  
+
